8cf304ca220dfe92d160f55e7df2ee9fe00eed39
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-06-28 03:43:52 +0000

    refactor: modularize the error message notification function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,29 @@
-const button = document.querySelector("#submit")
-const errorToast = document.querySelector(".error-message")
-const modal = document.querySelector(".modal")
-const icon = document.querySelector(".icon")
+import { Modal } from "../../modules/modal.js"
+import { Error } from "../../modules/error.js"
 
-button.addEventListener("click", handleClick)
-icon.addEventListener("click", handleModalClose)
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    handleModalClose()
+export const inputWeight = document.querySelector("#weight")
+export const inputHeight = document.querySelector("#height")
+const button = document.querySelector("#submit")
+
+button.onclick = (event) => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  if (weight === "" || height === "") {
+    Error.show()
+  } else {
+    const bmi = calculateBMI(Number(weight), Number(height))
+    displayResult(bmi)
   }
-})
+}
 
 const calculateBMI = (weight, height) => {
   return ((weight / (height * height)) * 10000).toFixed(1)
 }
 
 const displayResult = (bmi) => {
-  modal.classList.remove("hidden")
-  modal.querySelector("h2").innerHTML = `Your BMI is ${bmi} kg/m2.`
-}
-
-function handleClick(event) {
-  event.preventDefault()
-
-  const inputWeight = document.querySelector("#weight").value
-  const inputHeight = document.querySelector("#height").value
-
-  if (inputWeight === "" || inputHeight === "") {
-    handleErrorMessage()
-  } else {
-    const bmi = calculateBMI(Number(inputWeight), Number(inputHeight))
-    displayResult(bmi)
-  }
-}
-
-function handleErrorMessage() {
-  errorToast.classList.remove("hidden")
-
-  setTimeout(() => {
-    errorToast.classList.add("hidden")
-  }, 3000)
-}
-
-function handleModalClose() {
-  modal.classList.add("hidden")
+  Modal.open()
+  Modal.text.innerText = `Your BMI is ${bmi} kg/m2.`
 }
